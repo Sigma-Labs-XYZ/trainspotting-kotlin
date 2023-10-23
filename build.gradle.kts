20cb08e566bc30c159a61e1d7b4700cdfd33222d
Zak-Simjee <88289386+Zak-Simjee@users.noreply.github.com>
2023-10-23 10:50:23 +0100

    corrected dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.http4k:http4k-core:${extra["http4kVersion"]}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${extra["kotlinVersion"]}")
+    testImplementation("org.http4k:http4k-testing-approval:${extra["http4kVersion"]}")
+    testImplementation("org.http4k:http4k-testing-hamkrest:${extra["http4kVersion"]}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${extra["junitVersion"]}")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:${extra["junitVersion"]}")
 }
 
 tasks.test {
